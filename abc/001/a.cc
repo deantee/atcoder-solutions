1749a8eb2f4e5b6d575f630566e14f71f6f67a3c
dean <64692815+deant-digital@users.noreply.github.com>
2026-01-01 12:00:00 +0800

    feat: add ABC001A

---
+++ abc/001/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll h1, h2;
+  cin >> h1 >> h2;
+  cout << h1 - h2 << '\n';
+}
