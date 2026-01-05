63297fadfdda9bd89e4dfbb504081caa429761cd
dean <64692815+deant-digital@users.noreply.github.com>
2026-01-05 12:00:00 +0800

    feat: add ABC002A

---
+++ abc/002/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll x, y;
+  cin >> x >> y;
+  cout << max(x, y) << '\n';
+}
